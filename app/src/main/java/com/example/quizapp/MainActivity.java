f39fb8122e361a9a50ab68079d8ce4f40aa06160
hassanwaheed24 <hassanwaheed042@gmail.com>
2021-11-10 01:52:32 +0500

    to_source_code

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.quizapp;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
     TextView textView1;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +24,17 @@ public class MainActivity extends AppCompatActivity {
         textView.setText("Hassan Waheed");
          //Display the ID
         textView1 =  (TextView) findViewById(R.id.TextView2);
-        textView.setText("BITF18A524");
+        textView1.setText("BITF18A524");
+        //Button to open repository
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://github.com/HassanWaheed24/AssignmentNO3";
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
     }
-
 }
